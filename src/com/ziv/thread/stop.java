8807bc0e8f19053a8ffd348e3a800bd9f1f1879a
ziv-wong <zivwonghere@gmail.com>
2018-12-09 18:19:19 +0800

    修改暂停线程代码

--- src/com/ziv/thread/stop.java
+++ src/com/ziv/thread/stop.java
@@ -2,6 +2,6 @@ package com.ziv.thread;
 
 public class stop {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world！");
     }
 }
